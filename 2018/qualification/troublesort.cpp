db08f1b0cd26d06c878167d5f5c862293d3fd9a4
tonplamm <tonplam_kanokpol@hotmail.com>
2021-02-14 21:46:47 +0700

    2018 qualification troublesort

---
+++ 2018/qualification/troublesort.cpp
@@ -0,0 +1,20 @@
+/*
+
+test 1
+do trouble sort()
+loop check if current value is greater than value in the next index
+
+test 2
+seprate 2 arrays: array for even and odd index values
+do sort in each array
+combine and find ifcurrent value is greater than value in the next index
+
+test
+[8, 9, 7, 1, 2]
+even = [8, 7, 2]
+even.sort() -> [2, 7, 8]
+odd = [9, 1]
+odd.sort() -> [1, 9]
+combine = [2, 1, 7, 9, 8]
+find current is greater than next: found 2 > 1 so the first index is error!
+*/
